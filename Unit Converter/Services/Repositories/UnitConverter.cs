616db1e89e9a8af371d43dce200a9b904c485a66
Cris575 <cg691539@gmial.com>
2026-09-22 16:22:16 -0600

    chore: initialize ASP.NET Core MVC application

---
+++ Unit Converter/Services/Repositories/UnitConverter.cs
@@ -0,0 +1,15 @@
+﻿using Unit_Converter.Models;
+
+namespace Unit_Converter.Services.Repositories;
+public interface IUnitConverter
+{
+    public double ConvertLength(double value, double from, double to, IReadOnlyDictionary<string, double> values);
+}
+
+public class UnitConverter: IUnitConverter
+{
+    public double ConvertLength(double value, double from, double to, IReadOnlyDictionary<string, double> values)
+    {
+        return value / from;
+    }
+}
