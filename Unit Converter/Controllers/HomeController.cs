b9a3f801d2736d6e6013a753c059bf1698a62e9d
Cris575 <cg691539@gmial.com>
2026-09-24 12:16:49 -0600

    refactor: improve converter views and validation

--- Unit Converter/Controllers/HomeController.cs
+++ Unit Converter/Controllers/HomeController.cs
@@ -36,23 +36,38 @@ public class HomeController : Controller
     [HttpPost]
     public IActionResult UnitConvert(UnitConverterViewModel unitConverter)
     {
-        switch (unitConverter.View)
-        {
-            case "Length":
-                var lengthFactors = _unitFactorsProvider.GetLengthFactorsInMeters();
-                unitConverter.Value = _unitConverter.ConvertStandard(unitConverter, lengthFactors);
-                break;
+            switch (unitConverter.View)
+            {
+                case "Length":
+                    var lengthFactors = _unitFactorsProvider.GetLengthFactorsInMeters();
+                    if (!lengthFactors.ContainsKey(unitConverter.UnitFrom) ||
+                        !lengthFactors.ContainsKey(unitConverter.UnitTo))
+                    {
+                        return View("Index", unitConverter);
+                    }
+                    unitConverter.Value = Math.Round(_unitConverter.ConvertStandard(unitConverter, lengthFactors),4);
+                    break;
 
-            case "Weight":
-                var weightFactors = _unitFactorsProvider.GetWeightFactorsInKilograms();
-                unitConverter.Value = _unitConverter.ConvertStandard(unitConverter, weightFactors);
-                break;
+                case "Weight":
+                    var weightFactors = _unitFactorsProvider.GetWeightFactorsInKilograms();
+                    if (!weightFactors.ContainsKey(unitConverter.UnitFrom) ||
+                        !weightFactors.ContainsKey(unitConverter.UnitTo))
+                    {
+                        return View("Index", unitConverter);
+                    }
+                    unitConverter.Value = Math.Round(_unitConverter.ConvertStandard(unitConverter, weightFactors),4);
+                    break;
 
-            case "Temperatures":
-                var tempFactors = _unitFactorsProvider.GetTemperaturesFactorsInCelsius();
-                unitConverter.Value = _unitConverter.ConvertTemperature(unitConverter, tempFactors);
-                break;
-        }
+                case "Temperatures":
+                    var tempFactors = _unitFactorsProvider.GetTemperaturesFactorsInCelsius();
+                    if (!tempFactors.ContainsKey(unitConverter.UnitFrom) ||
+                        !tempFactors.ContainsKey(unitConverter.UnitTo))
+                    {
+                        return View("Index", unitConverter);
+                    }
+                    unitConverter.Value = Math.Round(_unitConverter.ConvertTemperature(unitConverter, tempFactors),4);
+                    break;
+            }
 
         return View("Index", unitConverter);
     }
@@ -63,4 +78,5 @@ public class HomeController : Controller
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 
+    
 }
--- Unit Converter/Services/UnitFactorsProvider.cs
+++ Unit Converter/Services/UnitFactorsProvider.cs
@@ -11,25 +11,25 @@ public class UnitFactorsProvider : IUnitFactorsProvider
 {
     private static readonly Dictionary<string, double> Lengths = new()
     {
-        { "Millimeter", 0.001 },
-        { "Centimeter", 0.01 },
-        { "Meter", 1.0 },
-        { "Kilometer", 1000.0 },
-        { "Inch", 0.0254 },
-        { "Foot", 0.3048 },
-        { "Yard", 0.9144 },
-        { "Mile", 1609.34 }
+        { "mm", 0.001 },
+        { "cm", 0.01 },
+        { "m", 1.0 },
+        { "km", 1000.0 },
+        { "in", 0.0254 },
+        { "ft", 0.3048 },
+        { "yd", 0.9144 },
+        { "mi", 1609.34 }
     };
 
     public IReadOnlyDictionary<string, double> GetLengthFactorsInMeters() => Lengths;
     
     private static readonly Dictionary<string, double> Weight = new()
     {
-        { "Milligram", 0.000001 },
-        { "Gram", 0.001 },
-        { "Kilogram", 1.0 },
-        { "Ounce", 0.0283495 },
-        { "Pound", 0.453592 },
+        { "mg", 0.000001 },
+        { "g", 0.001 },
+        { "kg", 1.0 },
+        { "oz", 0.0283495 },
+        { "lb", 0.453592 },
     };
 
     public IReadOnlyDictionary<string, double> GetWeightFactorsInKilograms() => Weight;   
@@ -37,9 +37,9 @@ public class UnitFactorsProvider : IUnitFactorsProvider
     private static readonly Dictionary<string, (Func<double, double> ToCelsius, Func<double, double> FromCelsius)> Temperatures 
         = new()
         {
-            { "Celsius", (c => c, c => c) },
-            { "Fahrenheit", (f => (f - 32.0) * 5.0 / 9.0, c => (c * 9.0 / 5.0) + 32.0) },
-            { "Kelvin", (k => k - 273.15, c => c + 273.15) }
+            { "°C", (c => c, c => c) },
+            { "°F", (f => (f - 32.0) * 5.0 / 9.0, c => (c * 9.0 / 5.0) + 32.0) },
+            { "K", (k => k - 273.15, c => c + 273.15) }
         };
 
     public IReadOnlyDictionary<string, (Func<double, double> ToCelsius, Func<double, double> FromCelsius)> GetTemperaturesFactorsInCelsius() => Temperatures;
